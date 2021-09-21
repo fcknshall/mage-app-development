4cd7552e728147a049a13c3066a2120648e9b767
Lenxza <87641634+Lenxza@users.noreply.github.com>
2021-09-21 14:36:48 +0700

    commit

--- lib/loginPage/login.dart
+++ lib/loginPage/login.dart
@@ -14,6 +14,7 @@ class _LoginState extends State<Login> {
       theme: ThemeData(
           scaffoldBackgroundColor: const Color.fromRGBO(73, 97, 222, 100)),
       home: Scaffold(
+        //AppBar
         appBar: AppBar(
           title: const Text("Login Page"),
           automaticallyImplyLeading: true,
@@ -25,14 +26,16 @@ class _LoginState extends State<Login> {
         body: SingleChildScrollView(
           child: Column(
             children: <Widget>[
+              //Email
               const Padding(
                 padding:
-                    EdgeInsets.only(left: 1, right: 250, top: 100, bottom: 0),
+                    EdgeInsets.only(left: 1, right: 320, top: 100, bottom: 0),
                 child: Text(
-                  "Login Quarantips",
+                  "E-Mail",
                   style: TextStyle(color: Colors.white),
                 ),
               ),
+              //Field Email
               Padding(
                 padding: const EdgeInsets.only(
                     left: 15.0, right: 15.0, top: 10, bottom: 0),
@@ -45,11 +48,18 @@ class _LoginState extends State<Login> {
                     fillColor: Color.fromRGBO(38, 61, 177, 100),
                     border: OutlineInputBorder(borderSide: BorderSide()),
                     labelStyle: TextStyle(color: Colors.white),
-                    labelText: 'Username',
-                    hintText: 'Masukan username',
+                    labelText: 'Masukan email anda',
+                    hintText: 'Masukan email anda',
                   ),
                 ),
               ),
+              //Password
+              const Padding(
+                  padding:
+                      EdgeInsets.only(left: 1, right: 300, top: 100, bottom: 0),
+                  child:
+                      Text("Password", style: TextStyle(color: Colors.white))),
+              //Password Field
               Padding(
                 padding: const EdgeInsets.only(
                     left: 15.0, right: 15.0, top: 10, bottom: 0),
@@ -66,14 +76,20 @@ class _LoginState extends State<Login> {
                       labelStyle: TextStyle(color: Colors.white)),
                 ),
               ),
+              //Continue
               Padding(
-                padding: const EdgeInsets.fromLTRB(100, 380, 100, 0),
+                padding: const EdgeInsets.fromLTRB(40, 280, 40, 0),
                 child: ElevatedButton(
                   style: ButtonStyle(
+                      fixedSize: MaterialStateProperty.all(const Size(350, 50)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              side: const BorderSide(color: Colors.white))),
                       backgroundColor: MaterialStateProperty.all<Color>(
                           const Color.fromRGBO(38, 61, 177, 100))),
                   onPressed: null,
-                  child: const Text("Lanjutkan"),
+                  child: const Text("Continue"),
                 ),
               ),
             ],
